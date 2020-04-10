9ba266619d664adee2561d975d7a2fd0c5ffb1d1
guanghao0419@outlook.com <1936381013@qq.com>
2020-04-09 23:15:18 -0700

    fix bug after refresh

--- check_instacart.py
+++ check_instacart.py
@@ -9,8 +9,8 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 chromedriver = "./chromedriver"
 
 # instacart credentials
-username = "fake@email"
-password = "00000"
+username = "fake@mail"
+password = "0000000"
 
 # hyper parameter
 time_lapse = 2.0
@@ -57,18 +57,30 @@ def check_slots():
     print('Redirecting to Ralphs ...')
     print('Step 1: Go to cart .....')
 
-    driver.find_elements_by_tag_name('button')[0].click()   
-    time.sleep(time_lapse*3)
+    but_lists = driver.find_elements_by_tag_name('button')  
+    cart_button = None
+    for item in but_lists:
+        if(item.text[2:6]=='Cart'):
+            cart_button = item
+            break    
+    if cart_button!=None:
+        cart_button.click()
+    else:
+        raise ValueError("Didn't find cart button")
+    time.sleep(time_lapse*3)    
 
     print('Step 2: Check out page .....')
-    driver.get('https://www.instacart.com/store/checkout_v3')  
-    time.sleep(time_lapse*3)
-            
+    try:
+        driver.find_element_by_xpath("//a[@href='checkout_v3']").click()
+    except:
+        raise ValueError("Not found check out button")
+
+    time.sleep(time_lapse*3)            
     print('Step 3: Collecting delivery availability .....')
     slots_available = False
+    no_delivery_image = "https://d2guulkeunn7d8.cloudfront.net/assets/modules/errors/heavy_load-9c583d42cc391f1ade7b077ef715e4c389741570579585b13ee7a27b169905b4.png"
 
     while not slots_available:
-        no_delivery_image = "https://d2guulkeunn7d8.cloudfront.net/assets/modules/errors/heavy_load-9c583d42cc391f1ade7b077ef715e4c389741570579585b13ee7a27b169905b4.png"
         image_set = set()
         tmp_list = driver.find_elements_by_tag_name('img')
         for item in tmp_list:
@@ -85,6 +97,7 @@ def check_slots():
             print('Cur time: {}, No slots available. Sleeping ...'.format(display_time()))
             time.sleep(60)
             driver.refresh()
+            time.sleep(time_lapse*3)
     
     time.sleep(300)
     terminate(driver)
